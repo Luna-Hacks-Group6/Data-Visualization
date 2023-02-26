53beb7b27859e34e0947568590e02bcff6609a12
Isaac Ngatia <isaacngatiawachira@gmail.com>
2023-02-26 06:16:39 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+X= [20,30,47,78,765,89,4]
+y = [33,5,72,366,5,4,66]
+
+
+
+
+plt.plot(X,y)
+plt.xlabel('X - axis')
+plt.ylabel('Y - axis')
+plt.title('Example')
+plt.legend('temp')
+
+plt.show()
